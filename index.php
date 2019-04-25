532c8e2f468cdad45343714e0307284f2ec561ee
Jessica Mitchell <jmmitchell11@yahoo.com>
2019-04-25 12:54:20 -0500

    update index format

--- index.php
+++ index.php
@@ -7,11 +7,13 @@
     <title>Stew's Smoke Shack</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/stews.css">
-
 </head>
+
 <body>
+
     <?php require 'base/public/navigation.php'; ?>
-    <div class="slider">
+
+    <div class="slider"> <!-- Replace images -->
         <img class="mySlides" src="images/banner.jpg" style="width:100%">
         <img class="mySlides" src="images/carryout.jpg" style="width:100%">
         <img class="mySlides" src="images/delivery.jpg" style="width:100%">
@@ -33,32 +35,26 @@
         <p>LOCATED IN DES MOINES, IOWA</p>
         <img src="images/store-front.jpg" alt="Stew's Smoke Shack"/>
     </div>
+
     <div class="cta-banner">
         <a href="#"><img src="images/banner.jpg" width="100%"/>
             <div class="centered">DELIVERY NOW AVAILABLE</div></a>
     </div>
+
     <div class="announcement-wrapper">
         <p>ANNOUNCEMENTS</p>
-
         <div class="temp"></div> <!-- temp container -->
     </div>
+
     <div class="about-wrapper">
         <p>WHO'S STEW?</p>
         <p>Stephen 'Stew' Stewart previously owned a patent design company before graduating from the Iowa Culinary Institute at DMACC. He then later received a scholarship for an internship at France. After his internship, Stephen returned to Iowa and worked at Edgewater retirement community in West Des Moines as a chef. Stephen is now the current owner of Smoke Shack by Stew's Que Catering formerly known as Woody's Smoke Shack.</p>
         <button class="btn"><a href="#">CONTACT STEW</a></button>
-
-    
-
-
-
-
-
     </div>
 
-
-
     <?php require 'base/public/footer.php'; ?>
+
     <script src="js/slideshow.js"></script>
-    
+
 </body>
 </html>
