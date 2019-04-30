9c3972c7ccb2ff0caa63c2ce92844aa7258d4008
quangp789 <48969916+quangp789@users.noreply.github.com>
2019-04-30 16:31:19 -0500

    Add files via upload

--- base/public/navigation.php
+++ base/public/navigation.php
@@ -1,7 +1,7 @@
 <div class= "nav-container" id= "myTopNav">
   <div class="nav-item"><a href="#">Menu</a></div>
   <div class="nav-item"><a href="index.php#about">Who's Stew</a></div>
-  <a href="index.php"><img src="images/nav-logo.png" alt="Stew's Logo"/></a>
+  <a href="index.php" class="stewsLogo"><img src="images/nav-logo.png" alt="Stew's Logo"/></a>
   <div class="nav-item"><a href="contact.php">Contact</a></div>
   <div class="nav-item"><a href="delivery.php">Order Now</a></div>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
