531f3d1dab9617d2bf03032a02c62fc79454ade9
John <jdpivotjohn@gmail.com>
2019-04-30 16:35:24 -0500

    delivery page done

--- delivery.php
+++ delivery.php
@@ -10,37 +10,40 @@
 
 </head>
 <body>
-    <?php require 'base/public/navigation.php'; ?>
+		
+		<?php require 'base/public/navigation.php'; ?>
     
+		<h1 class="title_delivery">Stews Smoke Shack Delivery Options</h1>
+		<h2 class="phone_number_delivery">Stew's Smoke Shack Phone Number: (515) 277-0005</h2>
 
-    <div class="container">
+    <div class="delivery_container">
+	
+		<div class="delivery_items">
 
-            <h1 class="title_catering">Stews Smoke Shack Delivery Options</h1>
-		
-	  <h2><a href="https://www.mytown2go.com/order/restaurant/stews-smoke-shack/7630">MyTown2Go</a>:</h2>
-		
-		<p><img src="images/mytown.png" width="165" height="165" alt=""/></p>
-		
-	  <h2><a href="https://www.doordash.com/store/woodys-smoke-shack-des-moines-493596/">DoorDash</a>:</h2>
-		
-		<p><img src="images/doordash.png" width="350" height="154" alt=""/></p>
-		
-	  <h2><a href="https://www.grubhub.com/restaurant/stews-smoke-shack-2511-cottage-grove-ave-des-moines/1124348">GrubHub</a>:</h2>
-		
-		<p><img src="images/grubhub.png" width="447" height="108" alt=""/></p>
-		
-	  <h2><a href="https://fooddudesdelivery.com">FoodDudes</a>:</h2>
-		
-		<p><img src="images/food-dudes.png" width="400" height="131" alt=""/></p>
-		
-		<h2><a href="https://www.seamless.com/menu/stews-smoke-shack-2511-cottage-grove-ave-des-moines/1124348">Seamless</a>:</h2>
-		
-		<p><img src="images/seamless.png" width="480" height="237" alt=""/></p>
-		
-		<h2 class="phone_number_catering">Stew's Smoke Shack Phone Number: (515) 277-0005</h2>
+			<a href="https://www.mytown2go.com/order/restaurant/stews-smoke-shack/7630"><img src="optimized/mytown.png" alt="My Town 2 Go"/></a>
 
+		</div>
+
+		<div class="delivery_items">
+			
+			<a href="https://www.doordash.com/store/woodys-smoke-shack-des-moines-493596/"><img src="optimized/doordash.png" alt="Door Dash"/></a></p>
+			
+		</div>
+		
+		<div class="delivery_items">
+
+			<a href="https://www.grubhub.com/restaurant/stews-smoke-shack-2511-cottage-grove-ave-des-moines/1124348"><img src="optimized/grubhub.png" alt="Grub Hub"/></a>			
+					
+		</div>
+
+		<div class="delivery_items">
+
+			<a href="https://www.seamless.com/menu/stews-smoke-shack-2511-cottage-grove-ave-des-moines/1124348"><img src="optimized/seamless.png" alt="Seamless"/></a>
+		
+		</div>
 		
 		
+
 </div>
 
 
